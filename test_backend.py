fb437772d13db2d6dd6b73c8074e56301b50678c
Gabe <gabrieldowen@gmail.com>
2026-02-01 12:33:51 -0600

    update test script with clear instructions

--- test_backend.py
+++ test_backend.py
@@ -4,7 +4,7 @@ response = requests.post(
     'http://localhost:5678/webhook/log-workout',
     json={
         'text': 'Did bench press 185 pounds for 10 reps, 3 sets',
-        'api_key': 'iron_track_secret_2026_xyz789'
+        'api_key': 'REPLACE_WITH_YOUR_CUSTOM_API_KEY'
     }
 )
 
